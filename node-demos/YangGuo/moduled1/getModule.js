f634d56976e2df2465a4a66d4d6ecf58ae4a79e8
fengandzhy <fengandzhy@gmail.com>
2019-07-13 17:30:17 +1200

    improve the module demo

--- node-demos/YangGuo/moduled1/getModule.js
+++ node-demos/YangGuo/moduled1/getModule.js
@@ -1,3 +1,24 @@
+
+
 var myModule = require('./module.js');
 myModule.setName('Sam');
 myModule.getName();
+console.log('======');
+
+
+/**
+ * 从这里可以看出，这里的myModule的这个变量，其实就是module.js里面的那个module.exports的这个变量
+ * */
+console.log(myModule);
+
+
+/**
+ * 通过require来得到myModule之后就会被缓存，以后无论多少次require 得到的都是同一个module
+ * 当通过./调用时就表示相对路径，相对于使用require的这个文件的路径.
+ * 当通过/调用时就表示绝对路径
+ * 当既没有./也没有/就表示核心模块或者加载自mode_modules目录
+ *
+ * */
+var myModule1 = require('./module.js');
+console.log(myModule1==myModule);
+
--- node-demos/YangGuo/moduled1/module.js
+++ node-demos/YangGuo/moduled1/module.js
@@ -1,8 +1,17 @@
 var myName;
-exports.setName = function (username){
-    myName = username;
-}
-exports.getName = function(){
-    console.log('My name is '+myName);
-}
+// exports.setName = function (username){
+//     myName = username;
+// }
+// exports.getName = function(){
+//     console.log('My name is '+myName);
+// }
 
+module.exports = {
+    setName:function (username) {
+        myName = username;
+    },
+    getName:function () {
+        console.log('My name is '+myName);
+    }
+}
+console.log(module.exports);
