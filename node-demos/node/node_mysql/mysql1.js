6336e53f30f004c30c639d7787b9b83a3c282472
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-09-12 18:18:31 +0800

    mysql demo

---
+++ node-demos/node/node_mysql/mysql1.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'node',
+    port:'3306'
+});
+
+connection.connect(function (err){
+    if(err){
+        console.log('[query]-:'+err);
+        return;
+    }
+    console.log('connect succeed!');
+});
+
+// var insertsql = 'insert into user(username,password) values(?,?)';
+// var param = ['aaa','ass'];
+// connection.query(insertsql,param,function(err,rs){
+//     if(err){
+//         console.log('insert err:'+err.message);
+//         return;
+//     }
+//     console.log(rs);
+//     console.log('insert success!');
+// });
+
+var selectsql = 'select * from user';
+connection.query(selectsql,function(err,rs,fields){
+    if(err){
+        console.log('[query]-:'+err);
+        return;
+    }
+    for(var i=0;i<rs.length;i++){
+        console.log(rs[i].username);
+    }
+});
+
+connection.end(function (err){
+    if(err){
+        console.log('[query]-:'+err);
+        return;
+    }
+    console.log('end succeed!');
+});
