018fce99c4f220b8daf26428f27256dbde29cbc0
fengandzhy <fengandzhy@gmail.com>
2023-11-23 07:10:35 +1300

    new project

---
+++ node-demos/buffer/buffer-01.js
@@ -0,0 +1,14 @@
+
+/**
+ * 关于创建buffer 有以下这么几种方式
+ * */
+let buffer1 = Buffer.alloc(10);
+console.log(buffer1);
+
+let buffer2 = Buffer.allocUnsafe(1000);
+console.log(buffer2);
+
+let buf_3 = Buffer.from('hello');
+let buf_4 = Buffer.from([105, 108, 111, 118, 101, 121, 111, 117]);
+console.log(buf_3);
+console.log(buf_4);
