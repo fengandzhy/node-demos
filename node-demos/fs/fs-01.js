3c71704304ce1e9e7871c6e18cf47c2c9aa17952
fengandzhy <fengandzhy@gmail.com>
2023-12-05 21:18:32 +1300

    buffer-01.js

---
+++ node-demos/fs/fs-01.js
@@ -0,0 +1,22 @@
+
+/**
+ * 文件的写入
+ * */
+const fs = require('fs');
+
+/**
+ * fs.writeFile(file, data[, options], callback)
+ * file 文件名
+ * data 待写入的数据
+ * options 选项设置 （可选）
+ * callback 写入回调
+ *
+ *
+ * */
+fs.writeFile('./座右铭.txt','三人行，必有我师矣.', err=>{
+    if(err){
+        console.log("写入失败.");
+        return;
+    }
+    console.log('写入成功.');
+});
