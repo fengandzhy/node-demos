6135241398851e60a1a9ad5814402f449105ac10
fengandzhy <fengandzhy@gmail.com>
2023-12-05 21:29:15 +1300

    buffer-01.js

--- node-demos/fs/fs-01.js
+++ node-demos/fs/fs-01.js
@@ -1,4 +1,3 @@
-
 /**
  * 文件的写入
  * */
@@ -13,8 +12,8 @@ const fs = require('fs');
  *
  * 这是个异步写入过程
  * */
-fs.writeFile('./座右铭.txt','三人行，必有我师矣.', err=>{
-    if(err){
+fs.writeFile('./座右铭.txt', '三人行，必有我师矣.', err => {
+    if (err) {
         console.log("写入失败.");
         return;
     }
--- node-demos/fs/fs-02.js
+++ node-demos/fs/fs-02.js
@@ -1,9 +1,8 @@
-
 /**
  * 同步写入文件
  * */
 const fs = require('fs');
 
-fs.writeFileSync('./data.txt','我来了');
+fs.writeFileSync('./data.txt', '我来了');
 
 console.log('执行到这里了.');
