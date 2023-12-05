2e763dab1cebf91f9f20bea28498e7abd6a662ef
fengandzhy <fengandzhy@gmail.com>
2023-12-06 08:09:22 +1300

    buffer-01.js

--- node-demos/fs/fs-16.js
+++ node-demos/fs/fs-16.js
@@ -5,6 +5,13 @@ const fs = require('fs');
 
 // 异步读取当前文件夹
 fs.readdir('./', (err, data) => {
-    if(err) throw err;
+    if(err){
+        console.log(err);
+        throw err;
+    }
     console.log(data);
 });
+
+// 同步读取当前文件夹
+const data = fs.readdirSync('./');
+console.log(data);
