e6551491f062c1d95cbd9f30d5cea878c55c9e99
fengandzhy <fengandzhy@gmail.com>
2023-12-06 07:55:16 +1300

    buffer-01.js

--- node-demos/fs/fs-13.js
+++ node-demos/fs/fs-13.js
@@ -1,6 +1,9 @@
 
+/**
+ * 递归创建文件夹
+ * */
+const fs = require('fs');
 
-//递归创建文件
 fs.mkdir('./a/b/c',{recursive: true}, err => {
     if (err) {
         console.log(err);
@@ -8,3 +11,6 @@ fs.mkdir('./a/b/c',{recursive: true}, err => {
     }
     console.log('创建成功.');
 });
+
+// 同步方式
+// fs.mkdirSync('./a/b/c', {recursive: true});
