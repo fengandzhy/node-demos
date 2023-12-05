3a66f58e586edec314442c5887e8361ed8b8224d
fengandzhy <fengandzhy@gmail.com>
2023-12-05 22:21:00 +1300

    buffer-01.js

--- node-demos/fs/fs-08.js
+++ node-demos/fs/fs-08.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const rs = fs.createReadStream('./孙文.jpg');
 
 // 必须绑定data事件, 才能取到所读数据
-rs.on('data', chunk =>{
+rs.on('data', chunk => {
     console.log(chunk.length);
 });
 
-rs.on('end', () =>{
-   console.log('读取完毕.');
+rs.on('end', () => {
+    console.log('读取完毕.');
 });
