03b61c0dc882a869449602db9171a1b989689fef
fengandzhy <fengandzhy@gmail.com>
2023-12-06 08:04:29 +1300

    buffer-01.js

---
+++ node-demos/fs/fs-15.js
@@ -0,0 +1,13 @@
+/**
+ * 递归删除文件夹
+ * */
+const fs = require('fs');
+
+// 异步递归删除文件夹, 这里只需要传入最上层的文件夹即可
+fs.rm('./a', {recursive: true}, err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('删除成功.');
+});
