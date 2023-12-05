a2ccc166d8248757e85a69d44c590e3c5c808cce
fengandzhy <fengandzhy@gmail.com>
2023-12-05 21:59:14 +1300

    buffer-01.js

---
+++ node-demos/fs/fs-06.js
@@ -0,0 +1,13 @@
+/**
+ * 文件读取
+ * */
+const fs = require('fs');
+fs.readFile('./观书有感.txt', (err, data) => {
+    if (err) {
+        console.log('读取错误');
+        return;
+    }
+    console.log(data.toString());
+});
+
+
