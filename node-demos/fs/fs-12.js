9f17c14eaac70ba421131bd3f1393bebb1da3ae3
fengandzhy <fengandzhy@gmail.com>
2023-12-06 07:53:09 +1300

    buffer-01.js

--- node-demos/fs/fs-12.js
+++ node-demos/fs/fs-12.js
@@ -4,13 +4,13 @@
 const fs = require('fs');
 
 // 如果html 已经存在, 则它会创建失败
-// fs.mkdir('./html', err => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log('创建成功.');
-// });
+fs.mkdir('./html', err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('创建成功.');
+});
 
 // 同步创建文件夹
 // fs.mkdirSync('./html');
