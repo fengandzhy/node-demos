1167636d0224af3485dc790327a5d8285b29c42c
fengandzhy <fengandzhy@gmail.com>
2023-12-06 22:31:30 +1300

    buffer-01.js

--- node-demos/fs/fs-10.js
+++ node-demos/fs/fs-10.js
@@ -4,9 +4,12 @@
 const fs = require('fs');
 
 // rename 实现文件的重命名和移动, 如果源文件和目标文件的路径一致，则表现为是重命名, 如果不一致则表现为是移动
-fs.rename('./观书有感.jpg', './观书有感.gif', err => {
-    if (err) {
-        console.log(err);
-    }
-});
+// fs.rename('./观书有感.jpg', './观书有感.gif', err => {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+// 同步重命名
+fs.renameSync('./观书有感.txt', './观书有感2.txt');
 
