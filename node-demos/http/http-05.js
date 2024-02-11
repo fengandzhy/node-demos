834a6b9cbc2cb6c3a1fed4791c88f1b8c7774c8d
fengandzhy <fengandzhy@gmail.com>
2024-02-11 18:14:40 +1300

    http-05.js

--- node-demos/http/http-05.js
+++ node-demos/http/http-05.js
@@ -5,10 +5,18 @@ const url = require('url');
  * 提取请求中的路径和查询字符串
  * */
 const server = http.createServer((request, response) => {
-    let url = new URL(request.url, 'http://127.0.0.1');
+    let url = new URL(request.url, 'http://127.0.0.1'); // 通过这种方式补齐完整的url http://localhost:9000/url?param1=1&param2=2
 
     const pathname = url.pathname;
     console.log(pathname);
+
+    const params = {};
+    url.searchParams.forEach((value,key) => {
+        params[key] = value;
+    });
+    console.log(params);
+
+
     response.end('end');
 });
 
