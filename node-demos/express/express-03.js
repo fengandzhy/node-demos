c42919f8af84ce033d12d40ede6bd6bc1b0f8e8f
fengandzhy <fengandzhy@gmail.com>
2024-02-25 16:00:13 +1300

    express-03.js

---
+++ node-demos/express/express-03.js
@@ -0,0 +1,17 @@
+const exp = require('express');
+
+const app = exp();
+
+/**
+ * 对于直接把参数写在路径上的方式, 就用这种方式来获取.
+ * */
+app.get('/user/list/:id/:base', (request, response) => {
+    console.log(request.params.id);
+    console.log(request.params.base);
+    response.setHeader('content-type', 'text/html;charset=utf-8');
+    response.end('商品详情');
+});
+
+app.listen(9000, () =>{
+    console.log('The server is running...');
+});
