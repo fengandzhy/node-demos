7bf5895e58d0dfe1235fed7b0b32b01be1c58d6d
fengandzhy <fengandzhy@gmail.com>
2024-02-25 15:45:18 +1300

    express-02.js

---
+++ node-demos/express/express-02.js
@@ -0,0 +1,16 @@
+const exp = require('express');
+
+const app = exp();
+
+app.get('/user/list', (request, response) => {
+
+    console.log(request.query);//获取查询字符串
+    console.log(request.get('host')); // 获取指定的请求头
+
+    response.end('Hello, Express'); // 如果不写response.end浏览器会一直不停地转
+});
+
+app.listen(9000, () =>{
+    console.log('The server is running...');
+});
+
