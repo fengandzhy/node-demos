fce878c3b896d69aa819fe8a2e805fa0574b9d4d
fengandzhy <fengandzhy@gmail.com>
2024-03-28 20:23:45 +1300

    express-route-parameter01.js

--- node-demos/express/express-request-body01.js
+++ node-demos/express/express-request-body01.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 /**
  * 使用express.urlencoded中间件来解析URL编码的请求体
@@ -8,16 +9,23 @@ const path = require('path');
  * */
 app.use(express.urlencoded({ extended: true }));
 
+//解析 JSON 格式的请求体的中间件
+// const jsonParser = bodyParser.json()
+
+//解析 querystring 格式请求体的中间件
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 /**
  * 使用express.json中间件来解析JSON格式的请求体
  * */
 app.use(express.json());
 
+//response.sendFile 响应文件内容
 app.get('/home',(request, response) => {
     response.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
-app.post('/login',(request, response) => {
+app.post('/login',urlencodedParser,(request, response) => {
     response.send(request.body);
 });
 
