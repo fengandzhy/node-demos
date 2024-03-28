33f320a89d3ae653e2053628d364a3293cf5acd1
fengandzhy <fengandzhy@gmail.com>
2024-03-28 20:16:34 +1300

    express-route-parameter01.js

--- node-demos/express/express-static-middleware01.js
+++ node-demos/express/express-static-middleware01.js
@@ -5,7 +5,7 @@ const app = exp();
  * http://localhost:9000/index.html 可以直接访问到 public 下面的 index.html.
  * 由于index.html是默认的路径, 所以 http://localhost:9000 也可以直接访问
  * */
-app.use(exp.static('./public'));
+app.use(exp.static('./public')); // 静态资源中间健
 
 app.get('/home',(request, response) => {
     response.send('网站首页');
