63f144be7f9be220bfcff237fe4b1983e9c84c5c
fengandzhy <fengandzhy@gmail.com>
2024-03-29 16:31:59 +1300

    new myapp

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,33 @@ router.get('/', function(req, res, next) {
 
 router.get('/upload', (req,res) =>{
   res.render('upload')
+});
+
+//处理文件上传
+router.post('/upload', (req, res) => {
+  //创建 form 对象
+  const form = new formidable.IncomingForm({
+    multiples: true ,
+    //设置上传文件的保存目录
+    uploadDir: __dirname + '/../public/images',
+    //保持文件后缀
+    keepExtensions: true
+  });
+  //解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // console.log(fields);// text  radio  checkbox  select
+    // console.log(files); // file
+
+    //服务器保存该图片的访问 URL
+    // /images/8ad3d5e36012212ba7642c000.jpg
+    let url = '/images/' + files.portrait.newFilename;// 将来将此数据保存在数据库中
+
+    res.send(url);
+  });
 });
 
 module.exports = router;
