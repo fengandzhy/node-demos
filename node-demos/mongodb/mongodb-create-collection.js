e3bb95ad4c4e94bbcff4aa286c90e9ddae0fe27f
fengandzhy <fengandzhy@gmail.com>
2024-03-30 17:54:20 +1300

    mongodb-create-collection.js

--- node-demos/mongodb/mongodb-create-collection.js
+++ node-demos/mongodb/mongodb-create-collection.js
@@ -14,7 +14,10 @@ mongoose.connection.once('open', async () => {
     //5. 创建文档的结构对象
     //设置集合中文档的属性以及属性值的类型
     let BookSchema = new mongoose.Schema({
-        name: String,
+        name: {
+            type: String,
+            required: true // 该属性必须不能为空
+        },
         author: String,
         price: Number
     });
